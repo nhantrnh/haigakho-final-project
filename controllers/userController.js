2e37eacbf6a0f737b1922cc56df4abd26bcd4132
nhantrnh <nhantrnh03@gmail.com>
2024-11-20 21:56:29 +0700

    add promotion and login (not fix yet)

--- controllers/userController.js
+++ controllers/userController.js
@@ -27,3 +27,35 @@ exports.signup = async (req, res) => {
 exports.getSignIn = (req, res) => {
   res.render("users/signin");
 };
+
+exports.signin = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(401).json({ message: "Tên đăng nhập không đúng." });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Mật khẩu không đúng." });
+    }
+
+    req.session.userId = user._id;
+    res.status(200).json({ message: "Đăng nhập thành công." });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Lỗi máy chủ." });
+  }
+};
+
+exports.signout = (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      return res.status(500).send("Không thể đăng xuất");
+    }
+    res.redirect("/signin");
+  });
+};
