88e576a7a07b50918879b8176f857408151e0e8d
nhantrnh <nhantrnh03@gmail.com>
2025-01-14 15:01:24 +0700

    fix view of some page án structure of them

--- routes/products.js
+++
@@ -1,9 +0,0 @@
-// routes/products.js
-const express = require("express");
-const router = express.Router();
-const productController = require("../controllers/productController");
-
-router.get("/", productController.getProducts);
-router.get("/:id", productController.getProductDetail);
-
-module.exports = router;
