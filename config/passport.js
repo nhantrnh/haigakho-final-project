3938207080b20fe7d035bf1bdd8ea92c0bd63345
nhantrnh <nhantrnh03@gmail.com>
2025-01-16 06:51:24 +0700

    fix ci.yml

--- config/passport.js
+++ config/passport.js
@@ -73,9 +73,9 @@ module.exports = function (passport) {
   passport.use(
     new FacebookStrategy(
       {
-        clientID: process.env.FACEBOOK_APP_ID,
-        clientSecret: process.env.FACEBOOK_APP_SECRET,
-        callbackURL: process.env.FACEBOOK_CALLBACK_URL,
+        clientIDFB: process.env.FACEBOOK_APP_ID,
+        clientSecretFB: process.env.FACEBOOK_APP_SECRET,
+        callbackURLFB: process.env.FACEBOOK_CALLBACK_URL,
         profileFields: ['id', 'displayName', 'email', 'photos'],
       },
       async (accessToken, refreshToken, profile, done) => {
