fd6f61406486052cecf35b23d1aa70d4620f9ded
nhantrnh <nhantrnh03@gmail.com>
2025-01-16 06:55:34 +0700

    fix something

--- index.js
+++ index.js
@@ -61,10 +61,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views")); // Add this line
 
 // MongoDB connection
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test");;
 
 // Routes
 app.use("/", require("./routes/pages"));
